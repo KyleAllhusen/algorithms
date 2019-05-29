c8851a1893ea77ab40dcd0e2d42af3c6c51d6b95
Kyle <kyle_allhusen@my.uri.edu>
2019-05-29 18:47:30 -0400

    WIP functions

--- source/functions.cpp
+++ source/functions.cpp
@@ -46,6 +46,16 @@ int max(int x, int y, int z) {
   return max(x, max(y, z));
 }
 
+int max(int arr[], int n) {
+  int ans = arr[0];
+  for(int i = 1; i < n; i++) {
+    if(arr[i] > ans) {
+      ans = arr[i];
+    }
+  }
+  return ans;    
+}
+
 int min(int x, int y) {
   return (x < y) ? x : y;
 }
@@ -54,10 +64,79 @@ int min(int x, int y, int z) {
   return min(x, min(y, z));
 }
 
+int min(int arr[], int n) {
+  int ans = arr[0];
+  for(int i = 1; i < n; i++) {
+    if(arr[i] < ans) {
+      ans = arr[i];
+    }
+  }
+  return ans;
+}
+
 int round(double x, char c) {
   if (x > 0) {
     return (c == 'd') ? (int)x : (int)x + 1;
   } else {
     return (c == 'd') ? (int)x - 1 : (int)x;
   }
+}
+
+bool all(bool arr[], int n) {
+  for(int i = 0; i < n; i++){
+    if (!arr[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+bool any(bool arr[], int n) {
+    for(int i = 0; i < n; i++){
+    if (arr[i]){
+      return true;
+    }
+  }
+  return false;
+}
+
+int frequency(int arr[], int n, int x) {
+  int count = 0;
+  for(int i = 0; i < n; i++) {
+    if(arr[i] == x) {
+      count++;
+    }
+  }
+  return count;
+}
+
+long sum(int arr[], int n) {
+  long sum = 0;
+  for(int i = 0; i < n; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+int index(char str[], char substr[]) {
+  
+}
+
+bool is_sorted(int arr[], int n) {
+  int i = 0;
+  if(arr[i] <= arr[i + 1]) {
+    for(i; i < n; i++) {
+      if(!(arr[i] > arr[i + 1])){
+        return false;
+      }
+    } 
+  }
+  else if(arr[i] >= arr[i + 1]) {
+    for(i; i < n; i++) {
+      if(!(arr[i] < arr[i + 1])) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
